a1cd20cb347fa85c110946142089660a1e4cf73e
Johan Segerlund <iamtwiz@gmail.com>
2014-10-03 15:12:06 +0200

    Added Activities

--- FleetSpeak/app/src/main/java/se/chalmers/fleetspeak/StartActivity.java
+++ FleetSpeak/app/src/main/java/se/chalmers/fleetspeak/StartActivity.java
@@ -1,11 +1,13 @@
 package se.chalmers.fleetspeak;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 
 import android.os.Bundle;
 import android.swedspot.automotiveapi.AutomotiveSignalId;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class StartActivity extends ActionBarActivity {
 
@@ -16,23 +18,23 @@ public class StartActivity extends ActionBarActivity {
         new TruckCommunicator().execute(AutomotiveSignalId.FMS_WHEEL_BASED_SPEED, AutomotiveSignalId.FMS_SELECTED_GEAR);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.start, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void goToBookmarks(View view) {
+        Intent getBookmarkIntent = new Intent(this, BookmarkActivity.class);
+        startActivity(getBookmarkIntent);
     }
 }
