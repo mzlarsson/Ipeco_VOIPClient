d7f9605a2733adf97d72eb6032385eb4714ec96f
Matz Larsson <matzl@student.chalmers.se>
2014-10-06 08:28:57 +0200

    Added serverside client code so it can handle commands separately from different users

--- FleetSpeak/Server/eclipse/src/se/chalmers/fleetspeak/tcp/CommandHandler.java
+++ FleetSpeak/Server/eclipse/src/se/chalmers/fleetspeak/tcp/CommandHandler.java
@@ -9,9 +9,12 @@ import java.util.List;
 import se.chalmers.fleetspeak.Client;
 
 public class CommandHandler extends TCPHandler{
+	
+	private CommandData cmd;
 
 	public CommandHandler(Socket clientSocket) {
 		super(clientSocket);
+		this.cmd = new CommandData();
 	}
 
 	@Override
@@ -25,14 +28,14 @@ public class CommandHandler extends TCPHandler{
 				if(message != null){
 					System.out.print("Command recived: ");
 					if(message.startsWith("/disconnect")){
-						Command.setValue("disconnect", true);
-					}else if(message.startsWith("/nick")){
-						Command.setValue("nick", message.substring(6));
+						setCommandValue("disconnect", true);
+					}else if(message.startsWith("/nick ")){
+						setCommandValue("nick", message.substring(6));
 					}else if(message.equals("/mute")){
 						System.out.println("Mute");
-						Command.setValue("mute", true);
+						setCommandValue("mute", true);
 					}else if(message.equals("/unmute")){
-						Command.setValue("mute", false);
+						setCommandValue("mute", false);
 						System.out.println("Unmute");
 					}else{
 						System.out.println("Unknown command. " + message);
@@ -43,6 +46,18 @@ public class CommandHandler extends TCPHandler{
 			System.out.println("[CommandHandler] "+e.getMessage());
 			notifyConnectionLost(this);
 		}
+	}
+	
+	public void addCommandListener(CommandListener listener){
+		cmd.addCommandListener(listener);
+	}
+	
+	public void removeCommandListener(CommandListener listener){
+		cmd.removeCommandListener(listener);
+	}
+	
+	private void setCommandValue(String command, Object value){
+		cmd.setValue(command, value);
 	}
 
 	@Override
