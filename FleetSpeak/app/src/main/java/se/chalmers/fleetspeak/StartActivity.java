f82145dc37023cdfac1c9e1e36ee0e4eb783f797
David Michaelsson <d.michaelsson@gmail.com>
2014-10-08 15:04:29 +0200

    App is running now(Bugfix)
    Though there might be undiscovered glitches

--- FleetSpeak/app/src/main/java/se/chalmers/fleetspeak/StartActivity.java
+++ FleetSpeak/app/src/main/java/se/chalmers/fleetspeak/StartActivity.java
@@ -1,19 +1,15 @@
 package se.chalmers.fleetspeak;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
-
 import android.os.Bundle;
 import android.swedspot.automotiveapi.AutomotiveSignalId;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class StartActivity extends ActionBarActivity {
     private EditText ipTextField;
@@ -27,18 +23,29 @@ public class StartActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
         new TruckCommunicator().execute(AutomotiveSignalId.FMS_WHEEL_BASED_SPEED, AutomotiveSignalId.FMS_SELECTED_GEAR);
-        ip = (EditText) findViewById(R.id.ipField);
-        port = (EditText) findViewById(R.id.portField);
 
+        ipTextField = (EditText) findViewById(R.id.ipField);
+        portTextField = (EditText) findViewById(R.id.portField);
+        userNameTextField = (EditText) findViewById(R.id.usernameField);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefEdit = prefs.edit();
+
+        String portNumber = prefs.getString("portNumber", "portNumber");
+        String username = prefs.getString("username", "username");
+        String ipAdress = prefs.getString("ipAdress", "ipAdress");
+
+        userNameTextField.setText(username);
+        portTextField.setText(portNumber);
+        ipTextField.setText(ipAdress);
+        savePrefs = (CheckBox) findViewById(R.id.saveUserPref);
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.start, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
@@ -47,20 +54,30 @@ public class StartActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
     public void goToBookmarks(View view) {
         Intent getBookmarkIntent = new Intent(this, BookmarkActivity.class);
         startActivity(getBookmarkIntent);
     }
 
     public void onConnectButtonClick(View view) {
-       String ipAdress = String.valueOf(ipTextField.getText());
-       int portNumber = Integer.parseInt(String.valueOf(portTextField.getText()));
-       //Connector.connect(ipAdress, portNumber);
+
+        String ipAdress = String.valueOf(ipTextField.getText());
+        int portNumber = Integer.parseInt(String.valueOf(portTextField.getText()));
+
+        //Connector.connect(ipAdress, portNumber);
         if(savePrefs.isChecked()){
             saveUsername(view);
         }
         Intent intent = new Intent(this,ChatRoomActivity.class);
         startActivity(intent);
+    }
+
+    public void saveUsername(View view){
+        String newUsername = String.valueOf(userNameTextField.getText());
+        String newIpAdress = String.valueOf(ipTextField.getText());
+        String newPortNumber = String.valueOf(portTextField.getText());
+        prefEdit.putString("username", newUsername);
+        prefEdit.putString("ipAdress", newIpAdress );
+        prefEdit.putString("portNumber", newPortNumber);
     }
 }
