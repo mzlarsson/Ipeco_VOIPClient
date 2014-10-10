63011bc6dfbebec152f4b42d85c2cd95ed4dc423
Patrik Haar <haar@student.chalmers.se>
2014-10-10 12:52:33 +0200

    Added the Log singleton

---
+++ FleetSpeak/Server/eclipse/src/se/chalmers/fleetspeak/Log.java
@@ -0,0 +1,30 @@
+package se.chalmers.fleetspeak;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Log {
+	private static Logger logger;
+	private static Log instance;
+	
+	public static void log(String msg) {
+		if (logger!=null) {
+			logger.log(Level.ALL, msg);
+		} else {
+			System.out.println(msg);
+		}
+	}
+	
+	public static void setupLogger(Logger logger) {
+		if (Log.logger==null) {
+			if (instance==null) {
+				instance = new Log();
+			}
+			instance.setLogger(logger);
+		}
+	}
+	
+	public void setLogger(Logger logger) {
+		Log.logger = logger;
+	}
+}
