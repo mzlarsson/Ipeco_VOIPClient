f6e0dd93a1c3d69ceccb8f8cbaf971733ceb92cb
Patrik Haar <haar@student.chalmers.se>
2014-10-10 15:46:35 +0200

    Added the Command class for data transfer between server and client

--- FleetSpeak/Server/eclipse/src/se/chalmers/fleetspeak/ServerGUI.java
+++ FleetSpeak/Server/eclipse/src/se/chalmers/fleetspeak/ServerGUI.java
@@ -1,28 +1,44 @@
 package se.chalmers.fleetspeak;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.text.DefaultCaret;
 
-public class ServerGUI extends JFrame implements ActionListener {
+public class ServerGUI extends JFrame implements ActionListener, KeyListener {
 
+	private static final long serialVersionUID = 1L;
 	private JLabel ip;
 	private JTextField tcpText;
-	private JTextField utpText;
+	private JTextField udpText;
 	private JButton stop;
 	private JButton start;
-	private boolean running;
+	JScrollPane scrollableText;
+	private JTextArea terminal;
+	private JTextField cmdLine;
+	
 	private static ServerMain server;
 	private static Thread serverThread;
+	private Logger log;
 
 	public static void main(String[] args) {
 		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
@@ -47,7 +63,8 @@ public class ServerGUI extends JFrame implements ActionListener {
 		setTitle("FleetSpeek Server");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
-		setLocation(500, 400);
+		setLocation(500, 200);
+		setupLogger();
 		populate();
 
 		pack();
@@ -62,18 +79,28 @@ public class ServerGUI extends JFrame implements ActionListener {
 			ip = new JLabel("Your @LAN-IP is: NOT FOUND");
 			e.printStackTrace();
 		}
-		add(ip, BorderLayout.NORTH);
+		// Panels containing several components for better structure.
+		JPanel west = new JPanel();
+		JPanel westBorder = new JPanel();
+		westBorder.setLayout(new BorderLayout());
+		JPanel center = new JPanel();
+		center.setLayout(new BorderLayout());
+		JPanel centerSouth = new JPanel();
+		centerSouth.setLayout(new BorderLayout());
+		
+		// The area for input of what ports to use.
+		westBorder.add(ip, BorderLayout.NORTH);
 		JPanel portsPanel = new JPanel();
 		portsPanel.setLayout(new GridLayout(2, 2));
 		JLabel tcpLabel = new JLabel("TCP-Port :");
 		tcpText = new JTextField("8867", 4);
-		JLabel utpLabel = new JLabel("UTP-Port :");
-		utpText = new JTextField("8868", 4);
+		JLabel utpLabel = new JLabel("UDP-Port :");
+		udpText = new JTextField("8868", 4);
 		portsPanel.add(tcpLabel);
 		portsPanel.add(tcpText);
 		portsPanel.add(utpLabel);
-		portsPanel.add(utpText);
-		add(portsPanel, BorderLayout.CENTER);
+		portsPanel.add(udpText);
+		westBorder.add(portsPanel, BorderLayout.CENTER);
 		JPanel buttons = new JPanel();
 		buttons.setLayout(new GridLayout(1, 2));
 		stop = new JButton("Stop");
@@ -83,38 +110,84 @@ public class ServerGUI extends JFrame implements ActionListener {
 		start.addActionListener(this);
 		buttons.add(stop);
 		buttons.add(start);
-		add(buttons, BorderLayout.SOUTH);
+		westBorder.add(buttons, BorderLayout.SOUTH);
+		
+		// The scrollable terminal window.
+		terminal = new JTextArea();
+		DefaultCaret caret = (DefaultCaret)terminal.getCaret();
+		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+		scrollableText = new JScrollPane(terminal);
+		terminal.setEditable(false);
+		scrollableText.setPreferredSize(new Dimension(500,500));
+		center.add(scrollableText, BorderLayout.CENTER);
+		west.add(westBorder);
+		
+		// The command input line.
+		JLabel cmdLineLabel = new JLabel("Command input:");
+		cmdLine = new JTextField(10);
+		cmdLine.addKeyListener(this);
+		centerSouth.add(cmdLineLabel, BorderLayout.WEST);
+		centerSouth.add(cmdLine, BorderLayout.CENTER);
+		center.add(centerSouth, BorderLayout.SOUTH);
+		add(west, BorderLayout.WEST);
+		add(center, BorderLayout.CENTER);
+	}
+
+	private void setupLogger() {
+		log = Logger.getGlobal();
+		log.setLevel(Level.ALL);
+		Handler logHandler = new Handler() {
+			
+			@Override
+			public void publish(LogRecord record) {
+				terminal.append(record.getMessage()+"\n");
+				scrollableText.getVerticalScrollBar().setValue(scrollableText.getVerticalScrollBar().getMaximum());
+			}
+			
+			@Override
+			public void flush() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void close() throws SecurityException {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		log.addHandler(logHandler);
+		Log.setupLogger(log);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == stop) {
-			if (running) {
-				if (server != null) {
-					try {
-						server.terminate();
-						serverThread.join();
-						server = null;
-						System.out.println("Server stopped.");
-					} catch (InterruptedException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
+			if (server != null) {
+				log.log(Level.ALL, "Terminating server...");
+				try {
+					server.terminate();
+					serverThread.join();
+					server = null;
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
 				}
+				log.log(Level.ALL, "Server stopped.");
 				tcpText.setEnabled(true);
-				utpText.setEnabled(true);
+				udpText.setEnabled(true);
 				start.setEnabled(true);
 				stop.setEnabled(false);
-				running = false;
 			}
 		} else if (e.getSource() == start) {
-			if (!running) {
+			if (server == null) {
 				serverThread = new Thread(new Runnable() {
 					public void run() {
 						try {
 							server = new ServerMain(Integer.parseInt(tcpText
 									.getText()),
-									Integer.parseInt(utpText.getText()));
+									Integer.parseInt(udpText.getText()));
+							server.start();
 						} catch (UnknownHostException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
@@ -123,11 +196,30 @@ public class ServerGUI extends JFrame implements ActionListener {
 				});
 				serverThread.start();
 				tcpText.setEnabled(false);
-				utpText.setEnabled(false);
+				udpText.setEnabled(false);
 				start.setEnabled(false);
 				stop.setEnabled(true);
-				running = true;
 			}
 		}
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode()==KeyEvent.VK_ENTER) {
+			log.log(Level.ALL, "COMMAND INPUT: " + cmdLine.getText());
+			cmdLine.setText("");
+		}		
 	}
 }
---
+++ FleetSpeak/app/src/main/java/se/chalmers/fleetspeak/Command.java
@@ -0,0 +1,31 @@
+package se.chalmers.fleetspeak;
+
+import java.io.Serializable;
+
+/**
+ * A class holding a command for communication between server and client
+ * Created by Patrik on 2014-10-10.
+ */
+public class Command implements Serializable {
+    private String command;
+    private Object key;
+    private Object value;
+
+    public Command(String command, Object key, Object value) {
+        this.command = command;
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public Object getKey() {
+        return key;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
