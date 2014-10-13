58918c896c39f15d9b71e25cf92f3ef17212a387
Patrik Haar <haar@student.chalmers.se>
2014-10-13 20:30:28 +0200

    Added an Eventbus

---
+++ FleetSpeak/Server/eclipse/src/se/chalmers/fleetspeak/eventbuss/EventBus.java
@@ -0,0 +1,71 @@
+package se.chalmers.fleetspeak.eventbuss;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A singleton that forwards events inside the server.
+ * @author Patrik
+ * @version 1.0
+ * 
+ */
+public class EventBus {
+	private static EventBus instance;
+	private List<IEventBusSubscriber> subscribers;
+
+	/**
+	 * Private constructor for singelton EventBus
+	 */
+	private EventBus() {
+		this.subscribers = new ArrayList<IEventBusSubscriber>();
+	}
+
+	/**
+	 * A getter method for the singelton instance. If the instance is null a new
+	 * EventBus is created.
+	 * 
+	 * @return the instance
+	 */
+	public static EventBus getInstance() {
+		if (instance == null) {
+			instance = new EventBus();
+		}
+		return instance;
+	}
+
+	/**
+	 * Adds a subscriber to the EventsBus' subscriber list.
+	 * 
+	 * @param subscriber
+	 *            that implements the IEventBusSubscriber interface.
+	 */
+	public void addSubscriber(IEventBusSubscriber subscriber) {
+		subscribers.add(subscriber);
+	}
+
+	/**
+	 * Removes a subscriber from the EventBus' subscriber list.
+	 * 
+	 * @param subscriber
+	 *            that implements the IEentBusSubscriber interface.
+	 */
+	public void removeSubscriber(IEventBusSubscriber subscriber) {
+		subscribers.remove(subscriber);
+	}
+
+	/**
+	 * Calls the EventPerformed in the subscribers and forwards the event to all
+	 * subscribers.
+	 * 
+	 * @param event
+	 *            that has been performed.
+	 */
+	public void fireEvent(EventBusEvent event) {
+		for (IEventBusSubscriber subscriber : subscribers) {
+			subscriber.eventPerformed(event);
+		}
+	}
+	public List<IEventBusSubscriber> getSubscribers(){
+		return subscribers;
+	}
+}
---
+++ FleetSpeak/Server/eclipse/src/se/chalmers/fleetspeak/eventbuss/IEventBusSubscriber.java
@@ -0,0 +1,19 @@
+package se.chalmers.fleetspeak.eventbuss;
+
+/**
+ * Interface for subscribers to the eventbus.
+ * @author Patrik
+ * @version 1.0
+ * 
+ */
+public interface IEventBusSubscriber {
+	/**
+	 * Method called when an event has been forwarded to the subscriber by the
+	 * EventBus.
+	 * 
+	 * @param event
+	 *            the event that has been performed.
+	 */
+	public void eventPerformed(EventBusEvent event);
+
+}
