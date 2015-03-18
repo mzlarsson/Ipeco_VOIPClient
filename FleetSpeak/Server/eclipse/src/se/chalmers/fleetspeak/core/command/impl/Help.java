5ca9ea2ac6eb89c568a7a2127ec836c9431846f5
Matz Larsson <matzlarsson94@gmail.com>
2015-03-18 15:04:53 +0100

    Added help commands (and fixed small description errors)

---
+++ FleetSpeak/Server/eclipse/src/se/chalmers/fleetspeak/core/command/impl/Help.java
@@ -0,0 +1,32 @@
+package se.chalmers.fleetspeak.core.command.impl;
+
+import se.chalmers.fleetspeak.core.command.Commands;
+import se.chalmers.fleetspeak.util.Log;
+
+public class Help extends BasicCommand{
+	
+	public Help(int execCode){
+		super(new CommandInfo("Help", "Help | Help [name]", "Shows help about all/a certain command", execCode));
+	}
+
+	@Override
+	public CommandResponse execute(int requester, Object... params){
+		if(params.length > 0){
+			String cmd = (String)params[0];
+			CommandInfo info = Commands.getInstance().findCommand(cmd);
+			if(info != null){
+				Log.logDebug("\t"+info.getFormat()+"\n\t\t"+info.getDescription());
+				return new CommandResponse(true, "The help information has been printed");
+			}else{
+				return new CommandResponse(false, "Unable to find command");
+			}
+		}else{
+			CommandInfo[] cmds = Commands.getInstance().getCommands();
+			for(CommandInfo info : cmds){
+				Log.logDebug("\t"+info.getFormat()+"\n\t\t"+info.getDescription());
+			}
+			return new CommandResponse(true, "The help information has been printed");
+		}
+	}
+
+}
