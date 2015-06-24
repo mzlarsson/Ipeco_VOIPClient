a4ae9029fbcbae207403ad9152d69aab15a78dde
Patrik Haar <haar@student.chalmers.se>
2015-06-24 18:33:01 +0200

    Added more functionality to the communicator and made more comments.

--- FleetSpeak/Server/eclipse/src/se/chalmers/fleetspeak/util/DatabaseCommunicator.java
+++ FleetSpeak/Server/eclipse/src/se/chalmers/fleetspeak/util/DatabaseCommunicator.java
@@ -23,6 +23,10 @@ public class DatabaseCommunicator {
 		conn = DatabaseConnector.initiateConnection();
 	}
 	
+	/**
+	 * Returns an instance of the DatabaseCommunicator.
+	 * @return An instance of the DatabaseCommunicator.
+	 */
 	public static DatabaseCommunicator getInstance() {
 		if (instance == null) {
 			instance = new DatabaseCommunicator();
@@ -31,41 +35,164 @@ public class DatabaseCommunicator {
 	}
 	
 	/**
+	 * Adds a user to the database.
+	 * @param username The unique username of the user.
+	 * @param alias The alias of the user.
+	 * @return null if successfully added, a string with the error if not.
+	 */
+	public String addUser(String username, String alias) {
+		String error = null;
+		Statement st = null;
+		try {
+			st = conn.createStatement();
+			int addedUsers = doUpdate("INSERT INTO users (username, alias) VALUES ('"
+					+ username + "', '" + alias + "');", st);
+			if (addedUsers != 1) {
+				error = "No errors but " + addedUsers + " users were added instead of 1.";
+			}
+		} catch (SQLException e) {
+			switch (e.getSQLState()) {
+			case "23505":
+				error = "Username '" + username + "' already exists.";
+				break;
+			default:
+				error = "Failed to add user with an unknown error code: " + e.getSQLState();
+				break;
+			}
+		}
+		try {
+			st.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return error;
+	}
+	
+	/**
+	 * Deletes the user with the given ID.
+	 * @param id The ID of the user to be deleted.
+	 * @return null if successfully removed, a string with the error if not.
+	 */
+	public String deleteUser(int id) {
+		return deleteUser(Integer.toString(id), true);
+	}
+	
+	/**
+	 * Deletes the user with the given username.
+	 * @param username The username of the user to be deleted.
+	 * @return null if successfully removed, a string with the error if not.
+	 */
+	public String deleteUser(String username) {
+		return deleteUser(username, false);
+	}
+	
+	/**
+	 * Deletes a user from the database.
+	 * @param idOrUsername The identifier of the user to be removed.
+	 * @param isID true if the identifier is an ID, false if it is a String.
+	 * @return null if successfully removed, a string with the error if not.
+	 */
+	private String deleteUser(String idOrUsername, boolean isID) {
+		String error = null;
+		Statement st = null;
+		try {
+			st = conn.createStatement();
+			int deletedUsers = doUpdate("DELETE FROM users WHERE "
+					+ (isID?"id":"username") + " = '" + idOrUsername + "';", st);
+			if (deletedUsers == 0) {
+				error = "The user " + (isID?"with ID ":"") + idOrUsername + " does not exist.";
+			} else if (deletedUsers != 1) {
+				error = "No errors but " + deletedUsers + " users were deleted instead of 1.";
+			}
+		} catch (SQLException e) {
+			switch (e.getSQLState()) {
+			default:
+				error = "Failed to delete user with an unknown error code: " + e.getSQLState();
+			}
+		}
+		try {
+			st.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return error;
+	}
+	
+	/**
 	 * Finds the information of the user from the database.
 	 * @param username The unique username of the user.
 	 * @return A UserInfo object with the information of the user if found, null if not found.
 	 */
 	public UserInfo findUser(String username) {
-		if (conn == null) {
-			throw new NullPointerException("[DatabaseCommunicator]: No connection to the database "
-					+ "has been established");
-		}
 		UserInfo user = null;
+		Statement st = null;
 		try {
-			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery("SELECT * FROM users "
-					+ "WHERE username = '"+username+"'");
+			st = conn.createStatement();
+			ResultSet rs = doQuery("SELECT * FROM users "
+					+ "WHERE username = '"+username+"'", st);
 			if (rs.next()) {
 				user = new UserInfo(rs.getInt(1), rs.getString(2), rs.getString(3));
 			}
 			else {
-				Log2.log(Level.INFO, ("[DatabaseCommunicator]: User with username \""
-						+ username + "\" was not found in the database."));
+				Log2.log(Level.INFO, ("[DatabaseCommunicator]: User with username '"
+						+ username + "' was not found in the database."));
 			}
 			rs.close();
+		} catch (SQLException e) {
+			Log2.log(Level.SEVERE, ("[DatabaseCommunicator]: Failed to read result from the found user in .findUser()."));
+			e.printStackTrace();
+		}
+		try {
 			st.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return user;
 	}
 	
+	/**
+	 * A query method to reduce code redundancy.
+	 * @param query The SQL-query to be sent to the server.
+	 * @param st The Statement to on which to execute the query.
+	 * @return The ResultSet from the processed query.
+	 * @throws SQLException The exception from a failed query for error handling.
+	 */
+	private ResultSet doQuery(String query, Statement st) throws SQLException {
+		ResultSet rs = null;
+		try {
+			rs = st.executeQuery(query);
+		} catch (SQLException e) {
+			throw e;
+		}
+		return rs;
+	}
+	
+	/**
+	 * An update method to reduce code redundancy.
+	 * @param query The SQL-query containing INSERT, UPDATE or DELETE to be sent to the server.
+	 * @param st The Statement to on which to execute the query.
+	 * @return The number of rows affected by the update.
+	 * @throws SQLException The exception from a failed query for error handling.
+	 */
+	private int doUpdate(String query, Statement st) throws SQLException {
+		int affectedRows = 0;
+		try {
+			affectedRows = st.executeUpdate(query);
+		} catch (SQLException e) {
+			throw e;
+		}
+		return affectedRows;
+	}
+	
+	/**
+	 * Closes all connections and frees all resources used.
+	 */
 	private void terminate() {
 		try {
 			if(conn!=null) {
 				conn.close();
 			}
+			instance = null;
 		} catch (SQLException e) {
 			Log2.log(Level.SEVERE, "[DatabaseCommunicator]: Failed to close the connection to the database.");
 			e.printStackTrace();
@@ -94,5 +221,30 @@ public class DatabaseCommunicator {
 			}
 			return connection;
 		}
+	}
+	
+	//FIXME temporary main method to easily test the class.
+	public static void main(String[] args) {
+		DatabaseCommunicator dc = DatabaseCommunicator.getInstance();
+		Scanner in = new Scanner(System.in);
+		String input = "";
+		while (!input.equals("q")) {
+			input = in.nextLine();
+			if (input.toLowerCase().startsWith("finduser")) {
+				String[] params = input.split(" ");
+				UserInfo result = dc.findUser(params[1]);
+				System.out.println(result!=null?result:"Not found");
+			} else if (input.toLowerCase().startsWith("adduser")) {
+				String[] params = input.split(" ");
+				String result = dc.addUser(params[1], params[2]);
+				System.out.println(result!=null?result:"OK");
+			} else if (input.toLowerCase().startsWith("deleteuser")) {
+				String[] params = input.split(" ");
+				String result = dc.deleteUser(params[1]);
+				System.out.println(result!=null?result:"OK");
+			}
+		}
+		in.close();
+		dc.terminate();
 	}
 }
--- FleetSpeak/Server/eclipse/src/se/chalmers/fleetspeak/util/UserInfo.java
+++ FleetSpeak/Server/eclipse/src/se/chalmers/fleetspeak/util/UserInfo.java
@@ -11,20 +11,38 @@ public class UserInfo {
 	private int id;
 	private String username, alias;
 	
+	/**
+	 * Create an object contain the information of a user.
+	 * @param id The ID of the user.
+	 * @param username The unique username of the user.
+	 * @param alias The alias of the user.
+	 */
 	public UserInfo(int id, String username, String alias) {
 		this.id = id;
 		this.username = username;
 		this.alias = alias;
 	}
 	
+	/**
+	 * Returns the users ID.
+	 * @return The users ID.
+	 */
 	public int getID() {
 		return id;
 	}
 	
+	/**
+	 * Returns the users unique username.
+	 * @return The users unique username.
+	 */
 	public String getUsername() {
 		return username;
 	}
 	
+	/**
+	 * Returns the users alias.
+	 * @return The users alias.
+	 */
 	public String getAlias() {
 		return alias;
 	}
