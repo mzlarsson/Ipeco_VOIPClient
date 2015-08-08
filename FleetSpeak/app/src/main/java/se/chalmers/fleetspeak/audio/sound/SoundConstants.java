d1a8e03de8d2c62c3208e3bc9ab2ba430ab3ec70
David Michaelsson <d.michaelsson@gmail.com>
2015-08-08 18:23:32 +0200

    Smallfix for performance

--- FleetSpeak/app/src/main/java/se/chalmers/fleetspeak/audio/sound/SoundConstants.java
+++ FleetSpeak/app/src/main/java/se/chalmers/fleetspeak/audio/sound/SoundConstants.java
@@ -19,12 +19,12 @@ public enum SoundConstants {
     OUTPUT_CHANNEL_CONFIG(AudioFormat.CHANNEL_OUT_MONO),
     SESSION_ID(AudioTrack.MODE_STREAM),
     STREAM_TYPE(AudioManager.STREAM_VOICE_CALL),
-    AUDIO_IN_BUFFER_SIZE(AudioRecord.getMinBufferSize(SAMPLE_RATE.value(), AudioFormat.CHANNEL_IN_MONO, AUDIO_ENCODING.value()) ),
-    AUDIO_OUT_BUFFER_SIZE(AudioRecord.getMinBufferSize(SAMPLE_RATE.value(), AudioFormat.CHANNEL_IN_MONO, AUDIO_ENCODING.value())),
-    BYTEBUFFER_IN_SIZE(AUDIO_IN_BUFFER_SIZE.value()*10),
-    BYTEBUFFER_OUT_SIZE(AUDIO_OUT_BUFFER_SIZE.value()*10),
+    AUDIO_IN_BUFFER_SIZE(AudioRecord.getMinBufferSize(SAMPLE_RATE.value(), AudioFormat.CHANNEL_IN_MONO, AUDIO_ENCODING.value())*3),
+    AUDIO_OUT_BUFFER_SIZE(AudioRecord.getMinBufferSize(SAMPLE_RATE.value(), AudioFormat.CHANNEL_IN_MONO, AUDIO_ENCODING.value())*3),
+    BYTEBUFFER_IN_SIZE(AUDIO_IN_BUFFER_SIZE.value()*3),
+    BYTEBUFFER_OUT_SIZE(AUDIO_OUT_BUFFER_SIZE.value()*3),
     FRAME_SIZE_MS(60),
-    INPUT_FRAME_SIZE(AudioRecord.getMinBufferSize(SAMPLE_RATE.value(),INPUT_CHANNEL_CONFIG.value(),AUDIO_ENCODING.value()));//Higher value (To a limit...) degrades sound quality and latency but eases on the performance.
+    INPUT_FRAME_SIZE((AudioRecord.getMinBufferSize(SAMPLE_RATE.value(),INPUT_CHANNEL_CONFIG.value(),AUDIO_ENCODING.value()))*3);//Higher value (To a limit...) degrades sound quality and latency but eases on the performance.
 
     private final int value;
 
