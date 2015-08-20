4dc5386ee144eb636d1a4ec3690a416460102ec3
Patrik Haar <haar@student.chalmers.se>
2015-08-20 18:02:16 +0200

    Added comments and UDP sending logic

--- Server/eclipse/src/se/chalmers/fleetspeak/network/udp/PacketReceiver.java
+++ Server/eclipse/src/se/chalmers/fleetspeak/network/udp/PacketReceiver.java
@@ -1,6 +1,15 @@
 package se.chalmers.fleetspeak.network.udp;
 
+/**
+ * Interface for classes able to receive packets in the form of a byte[].
+ *
+ * @author Patrik Haar
+ */
 public interface PacketReceiver {
 
+	/**
+	 * This method will be called every time a new packet is received.
+	 * @param packet the data of the packet.
+	 */
 	public void handlePacket(byte[] packet);
 }
