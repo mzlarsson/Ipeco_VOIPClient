3dbf0d8fdb7b5825ab4544a8a9705f90477a3c8d
Patrik Haar <haar@student.chalmers.se>
2015-08-22 17:53:42 +0200

    Added aditional RTP implementation

--- Server/eclipse/src/se/chalmers/fleetspeak/network/udp/RTPHandler.java
+++ Server/eclipse/src/se/chalmers/fleetspeak/network/udp/RTPHandler.java
@@ -19,47 +19,59 @@ public class RTPHandler implements PacketReceiver{
 		this.data = packet;
 	}
 	
-	public RTPHandler(byte[] data){
-		this.data = data;
-	}
-	
-	protected int getRTPVersion(){
+	private int getRTPVersion(){
 		return data[0]>>>6;
 	}
 	
-	protected boolean hasPadding(){
+	private boolean hasPadding(){
 		return (data[0]&0x20) > 0; 
 	}
 	
-	protected boolean hasExtension(){
+	private boolean hasExtension(){
 		return (data[0]&0x10) > 0;
 	}
 	
-	protected int getCrscCount(){
+	private int getCrscCount(){
 		return data[0]&0x0F;
 	}
 	
-	protected boolean isMarked(){
+	private boolean isMarked(){
 		return (data[1]>>>7) > 0;
 	}
 	
-	protected int getPayloadType(){
+	private int getPayloadType(){
 		return (data[1]&0x7F);
 	}
 	
-	protected short getSequenceNumber(){
+	private short getSequenceNumber(){
 		return (short)(Byte.toUnsignedInt(data[2])*256+data[3]);
 	}
 	
-	protected long getTimeStamp(){
+	private long getTimeStamp(){
 		return (long)Byte.toUnsignedInt(data[4])*16777216+Byte.toUnsignedInt(data[5])*65536+Byte.toUnsignedInt(data[6])*256+data[7];
 	}
 	
-	protected long getSourceID(){
+	private long getSourceID(){
 		return (long)Byte.toUnsignedInt(data[8])*16777216+Byte.toUnsignedInt(data[9])*65536+Byte.toUnsignedInt(data[10])*256+data[11];
 	}
 	
-	protected byte[] getData(){
+	private byte[] getData(){
 		return Arrays.copyOfRange(data, 12, 160);
+	}
+	
+	private class JitterBuffer {
+		
+		private long bufferTime;
+		
+		/**
+		 * Constructs a JitterBuffer which delays the media in favor of consistency
+		 * where packets have more time to arrive and can be sorted in the right order.
+		 * Higher buffer time will improve consistency on poor networks but will add a
+		 * delay to the processing of the media equal to the buffer time. 
+		 * @param bufferTime The delay the JitterBuffer has to work with in milliseconds.
+		 */
+		public JitterBuffer(long bufferTime) {
+			this.bufferTime = bufferTime;
+		}
 	}
 }
