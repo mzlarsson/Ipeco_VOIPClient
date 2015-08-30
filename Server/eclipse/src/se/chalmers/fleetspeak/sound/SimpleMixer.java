4227c2be0b575fd830a63bbb39466e4856f05f45
Matz Larsson <matzlarsson94@gmail.com>
2015-08-30 19:58:06 +0200

    Added major bug in mixer

--- Server/eclipse/src/se/chalmers/fleetspeak/sound/SimpleMixer.java
+++ Server/eclipse/src/se/chalmers/fleetspeak/sound/SimpleMixer.java
@@ -22,17 +22,32 @@ public class SimpleMixer extends AbstractMixer{
 		byte[][] data = getData();
 		int members = data.length;			//Used for clarity in the code.
 		
-		byte[][] mixed = new byte[members][data.length];
-		for(int i = 0; i<members; i++){																//For every member
-			for(int j = 0; j<mixed[i].length; j++){													//Mix every byte
-				for(int k = 0; k<members; k++){														//By traversing all members sound
-					if(data[k].length>j && k != i){													//Except themself (or empty signals)
-						mixed[i][j] = (byte) (mixed[i][j]+data[k][j]-mixed[i][j]*data[k][j]/127);		//And perform a+b-ab on the signal
+		if(data.length>1){
+			byte[][] mixed = new byte[members][getMaxDataLength(data)];
+			for(int i = 0; i<members; i++){																//For every member
+				for(int j = 0; j<mixed[i].length; j++){													//Mix every byte
+					for(int k = 0; k<members; k++){														//By traversing all members sound
+						if(data[k].length>j && k != i){													//Except themself (or empty signals)
+							mixed[i][j] = (byte) (mixed[i][j]+data[k][j]-mixed[i][j]*data[k][j]/127);		//And perform a+b-ab on the signal
+						}
 					}
 				}
 			}
+			
+			return mixed;
+		}else{
+			return new byte[data.length][0];
+		}
+	}
+	
+	private int getMaxDataLength(byte[][] data){
+		int length = 0;
+		for(int i = 0; i<data.length; i++){
+			if(data[i].length>length){
+				length = data[i].length;
+			}
 		}
 		
-		return mixed;
+		return length;
 	}
 }
